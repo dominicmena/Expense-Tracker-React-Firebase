6f73c5b9964cda5b522d0847e252b62477d030f6
dominicmena <mena.dominic@gmail.com>
2024-06-03 00:43:58 -0500

    creating design structure for expense page

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,4 +1,23 @@
 export const ExpenseTracker = () => {
 
-    return <div>hello world Expense Tracker</div>
+    return <div className="expense-tracker">
+
+        <div className="container">
+            <h1>Expense Tracker</h1>
+            <div className="balance">
+                <h3>Your Balance</h3>
+                <h2>$0.00</h2>
+            </div>
+            <div className="summary">
+                <div className="income">
+                    <h4>Income</h4>
+                    <p>$0.00</p>
+                </div>
+                <div className="expenses">
+                <h4>Expenses</h4>
+                    <p>$0.00</p>
+                </div>
+            </div>
+        </div>
+    </div>
 };
